743b16f8b2b8fc9b89efbcedbee291d9246cda72
IsaacL05 <isaaclee573@gmail.com>
2025-02-15 17:03:14 -0800

    Step 3 first draft

--- exotic_backend/api/views.py
+++ exotic_backend/api/views.py
@@ -13,6 +13,10 @@ import re
 import json
 import glob
 
+import urllib
+from urllib.request import urlopen
+from urllib.error import HTTPError
+
 class ConfigureExotic(APIView):
     def get(self, request):
         print("hi")
@@ -77,3 +81,94 @@ class ConfigureExotic(APIView):
             inits_file_exists = False
 
         return Response(status=status.HTTP_200_OK)
+
+    Telescope = 'MicroObservatory' #@param ["Select a Telescope", "MicroObservatory", "Exoplanet Watch .4 Meter"]
+    Star = '' #@param {type:"string"}
+    Target = Star.strip()
+    j = 0
+    while not os.path.exists(first_image):
+        first_image = verified_filepath + '/' + fits_list[j]
+        j += 1
+
+    def get_star_chart_urls(telescope, star_target):
+        if telescope == 'MicroObservatory':
+            t_fov=56.44
+            t_maglimit=15
+            t_resolution=150
+        elif telescope == 'Exoplanet Watch .4 Meter':
+            t_fov=38.42
+            t_maglimit=15
+            t_resolution=150
+        else:
+            # Should not get here
+            t_fov=56.44
+            t_maglimit=15
+            t_resolution=150
+        json_url = f"https://app.aavso.org/vsp/api/chart/?star={star_target}&scale=D&orientation=CCD&type=chart&fov={t_fov}&maglimit={t_maglimit}&resolution={t_resolution}&north=down&east=left&lines=True&format=json"
+        starchart_url = f"https://app.aavso.org/vsp/?star={star_target}&scale=D&orientation=CCD&type=chart&fov={t_fov}&maglimit={t_maglimit}&resolution={t_resolution}&north=down&east=left&lines=True"
+        return [json_url, starchart_url]
+
+    def get_star_chart_image_url(json_url):
+        with urllib.request.urlopen(json_url) as url:
+            starchart_data = json.load(url)
+            image_uri = starchart_data["image_uri"].split('?')[0]
+            return(image_uri)
+
+    if not inits_file_exists:
+        if Telescope != 'Select a Telescope' and Target:
+
+            starchart_image_url = ''
+            starchart_image_url_is_valid = False
+            prompt_for_url = True
+
+            starchart_urls = get_star_chart_urls(Telescope,Target)
+            try:
+                # Generate the starchart image url
+                starchart_image_url = get_star_chart_image_url(starchart_urls[0])
+                starchart_image_url_is_valid = True
+                prompt_for_url = False
+            except HTTPError:
+                prompt_for_url = True
+
+
+            while prompt_for_url:
+                starchart_image_url = input('Enter a valid starchart image URL and press return: ')
+                if starchart_image_url.startswith('https://') and starchart_image_url.endswith('png'):
+                    starchart_image_url_is_valid = True
+                    prompt_for_url = False
+                else:
+                    starchart_image_url_is_valid = False
+
+            if fits_files_found and starchart_image_url_is_valid:
+
+                # set up bokeh
+                bokeh.io.output_notebook()
+                sample_data = False
+
+                # show images
+                if first_image:
+                    obs = ""
+
+                    # request coordinates and verify the entries are valid
+                    success = False
+                    while not success:
+                        targ_coords = input("Enter coordinates for target star - in the format [424,286] - and press return:  ")
+
+                        # check syntax and coords
+                        targ_coords = targ_coords.strip()
+                        tc_syntax = re.search(r"\[\d+, ?\d+\]$", targ_coords)
+                        if tc_syntax:
+                            success = True
+
+                    # request coordinates and verify the entries are valid
+                    success = False
+                    while not success:
+                        comp_coords = input("Enter coordinates for the comparison stars - in the format [[326,365],[416,343]] - and press return:  ")
+
+                        # check syntax
+                        comp_coords = comp_coords.strip()
+                        cc_syntax = re.search(r"\[(\[\d+, ?\d+\],? ?)+\]$", comp_coords)
+                        if cc_syntax:
+                            success = True
+
+                    inits_file_path = make_inits_file(planetary_params, verified_filepath, output_dir, first_image, targ_coords, comp_coords, obs, aavso_obs_code, sec_obs_code, sample_data)
